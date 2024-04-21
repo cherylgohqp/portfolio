1b6328ffe7b21e8a3f362d4c07603666287d4eac
Cheryl <cheryl.qp.goh@lendlease.com>
2024-04-21 14:53:21 +0800

    Updated about me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,23 +7,44 @@ const AboutPage = () => {
       <div className={classes["about-me-container"]}>
         <div className={classes["overlay"]}>
           <div className={classes["content"]}>
-            {/* <div className={classes["picture"]}> */}
-            <span className={classes["paper-clip"]}></span>
             <Image
               src="/Me.jpg"
               className={classes["picture"]}
               alt="this is me"
               height={450}
-              width={950}
+              width={750}
             />
-            {/* </div> */}
             <div className={classes["section-container"]}>
-              <h3 className={classes["section-title"]}>About Me</h3>
-                <p>
-                  Hi there! I'm Cheryl, a front-end developer based in SG. I
-                  love building beautiful and responsive websites. When I'm not
-                  coding, you can find me dancing or exploring new places.
+              <h1 className={classes["section-title"]}>About Me</h1>
+                <p className={classes["about"]}>
+                Hi there, I'm Cheryl, a front end developer who loves bringing figma designs to life and building user-friendly websites and webapps.
+                <br></br><br></br>My coding journey began with Python and Java, and now I'm mainly working with ReactJS, typescript and SCSS.
+                <br></br><br></br>When I'm not coding, you might find me sweating it out at dance classes or going on nature trail walks!
+                <br></br><br></br> Some of my skills include:
                 </p>
+              <ul className={classes["skillsList"]}>
+                <li>
+                  <span>Figma</span>
+                </li>
+                <li>
+                  <span>Java</span>
+                </li>
+                <li>
+                  <span>JavaScript</span>
+                </li>
+                <li>
+                  <span>Python</span>
+                </li>
+                <li>
+                  <span>ReactJS</span>
+                </li>
+                <li>
+                  <span>TypeScript</span>
+                </li>
+                <li>
+                  <span>SCSS</span>
+                </li>
+            </ul>
             </div>
           </div>
         </div>
