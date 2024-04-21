cbfdcd29def1df32e0b50e0ade41378309325d26
Cheryl <cheryl.qp.goh@lendlease.com>
2024-04-21 13:50:38 +0800

    added photo and styling to about  me page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,33 @@
 import Image from "next/image";
-import styles from "../styles/AboutPage.module.css";
+import classes from "../styles/AboutPage.module.css";
 
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
+      <div className={classes["about-me-container"]}>
+        <div className={classes["overlay"]}>
+          <div className={classes["content"]}>
+            {/* <div className={classes["picture"]}> */}
+            <span className={classes["paper-clip"]}></span>
+            <Image
+              src="/Me.jpg"
+              className={classes["picture"]}
+              alt="this is me"
+              height={450}
+              width={950}
+            />
+            {/* </div> */}
+            <div className={classes["section-container"]}>
+              <h3 className={classes["section-title"]}>About Me</h3>
+                <p>
+                  Hi there! I'm Cheryl, a front-end developer based in SG. I
+                  love building beautiful and responsive websites. When I'm not
+                  coding, you can find me dancing or exploring new places.
+                </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
